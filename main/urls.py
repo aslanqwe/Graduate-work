0e74a034f7b11ef4e4c48b5db8d18bbdb9c429c7
Aslan Kobzhanov <aslankobzhanov6@gmail.com>
2023-05-23 01:27:55 +0600

    добавил переход по новостям и создание для них динамической страницы, но нужно поработать со стилями

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'home'),
     path('about/', views.about, name = 'about'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView, UpdateView
 from .models import Articles
 
 
@@ -9,3 +9,13 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html', {'title': 'О нас'})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'main/details_view.html'
+    context_object_name = 'article'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Новости'
+        return context
