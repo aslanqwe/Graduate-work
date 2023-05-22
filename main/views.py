c808e2a2f2a4aee22ac087250ee1597cbfecea7b
Aslan Kobzhanov <aslankobzhanov6@gmail.com>
2023-05-22 23:03:03 +0600

    исправил ссылку на статик в сеттингс, добавил страницы о нас

--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html', {'title': 'Детский сад "ДиАми"'})
+
+def about(request):
+    return render(request, 'main/about.html', {'title': 'О нас'})
