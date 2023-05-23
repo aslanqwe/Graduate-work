e28e0643c0ea9c190aac0cb09c4eb3da80010a4b
Aslan Kobzhanov <aslankobzhanov6@gmail.com>
2023-05-23 22:25:46 +0600

    решил удалить редактирование и удаление новостей с сайта, лучше делать через админку

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.index, name = 'home'),
     path('about/', views.about, name = 'about'),
     path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
-    path('<int:pk>/edit', views.NewsEditView.as_view(), name='news_edit')
 ]
--- main/views.py
+++ main/views.py
@@ -23,27 +23,3 @@ class NewsDetailView(DetailView):
         context['title'] = 'Новости'
         return context
 
-class NewsEditView(UpdateView):
-    model = Articles
-    template_name = 'main/edit.html'
-    form_class = ArticlesForm
-    context_object_name = 'article'
-    # перенаправит на ту же новость, которую редактировал
-    def get_success_url(self):
-        return reverse_lazy('news_detail', kwargs={'pk': self.object.pk})
-
-    def post(self, request, *args, **kwargs):
-        if "delete" in request.POST:
-            return self.delete(request, *args, **kwargs)
-        else:
-            return super().post(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.delete()
-        return redirect('home')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Редактирование статьи'
-        return context
